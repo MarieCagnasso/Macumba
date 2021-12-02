329545a4e079959fe617ca3e566cd3dd5b0a1439
mathieu.valette1 <mathieu.valette1@gmail.com>
2021-12-02 23:00:11 +0100

    ajout Rescue

--- app/model/Rescue.js
+++ app/model/Rescue.js
@@ -4,6 +4,9 @@ class Rescue{
         this.description = description;
         this.individualRescue = individualRescue;
     }
+    date;
+    description;
+    individualRescue;
 
 
     get date() {
