59589f60459a27f0c0bc3730c2bc94d1a0953f49
mathieu.valette1 <mathieu.valette1@gmail.com>
2021-12-02 22:39:51 +0100

    ajout testimony.js

---
+++ app/model/testimony.js
@@ -0,0 +1,14 @@
+class Testimony{
+    constructor(testimony) {
+        this._testimony = testimony;
+    }
+
+    get testimony() {
+        return this._testimony;
+    }
+
+    set testimony(value) {
+        this._testimony = value;
+    }
+}
+
